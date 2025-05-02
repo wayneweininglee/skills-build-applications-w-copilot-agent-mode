6087ae7b9ee3b7cc7fcabafaf6d71838bdc8da28
Wayne Lee <139816023+wayneweininglee@users.noreply.github.com>
2025-05-02 20:26:51 +0000

    Update views.py and settings.py to include codespace URL for API endpoints and allow host access

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-*(mam2619$2tt4bh8$dqql3@v5cm0jn#jcs(qk9q8b_v3ap+31
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'urban-guide-v6vprp5p7pgrcx5rg-8000.app.github.dev']
 
 
 # Application definition
